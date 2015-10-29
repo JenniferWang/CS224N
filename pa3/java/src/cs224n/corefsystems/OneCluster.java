e9b992590870341824bb4be19e2897c5bf7c226c
Jiyue Wang <jennifer.w0930@gmail.com>
2015-10-28 21:47:08 -0700

    ..test one cluster

--- pa3/java/src/cs224n/corefsystems/OneCluster.java
+++ pa3/java/src/cs224n/corefsystems/OneCluster.java
@@ -2,9 +2,11 @@ package cs224n.corefsystems;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.ArrayList;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
+import cs224n.coref.Mention;
 import cs224n.coref.Entity;
 import cs224n.util.Pair;
 
@@ -18,8 +20,15 @@ public class OneCluster implements CoreferenceSystem {
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		List<ClusteredMention> clusters = new ArrayList<ClusteredMention>();
+    List<Mention> mentions = doc.getMentions();
+    if (mentions.size() == 0) {
+		  return clusters;
+    }
+    ClusteredMention newCluster = mentions.get(0).markSingleton();
+    for (Mention m: mentions) {
+      clusters.add(m.markCoreferent(newCluster));
+    }
+    return clusters;
 	}
-
 }
